20a94a60cc39f7fc97a925cc3b0d7410a695b18d
amanqs <116822442+amanqs@users.noreply.github.com>
2022-12-12 21:05:33 +0700

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ MSG_PERMIT = (
  ⍟ 𝙹𝙰𝙳𝙸 𝚃𝚄𝙽𝙶𝙶𝚄 𝚂𝙰𝙼𝙿𝙰𝙸 𝙼𝙰𝙹𝙸𝙺𝙰𝙽 𝙶𝚄𝙰 𝙽𝙴𝚁𝙸𝙼𝙰 𝙿𝙴𝚂𝙰𝙽 𝙻𝚄
 ╔═════════════════════╗
 │ㅤㅤ𖣘 𝙿𝙴𝚂𝙰𝙽 𝙾𝚃𝙾𝙼𝙰𝚃𝙸𝚂 𖣘ㅤㅤ
-│ㅤㅤ   𖣘 𝙰𝚈𝙸𝙸𝙽 - 𝚄𝙱𝙾𝚃 𖣘ㅤㅤ
+│ㅤㅤ   𖣘 AMWANG - PYROBOT 𖣘ㅤㅤ
 ╚═════════════════════╝
 """
 )
@@ -26,14 +26,14 @@ MSG_PERMIT = (
 class Var:
     API_HASH = getenv("API_HASH")
     API_ID = int(getenv("API_ID", ""))
-    ALIVE_PIC = getenv("ALIVE_PIC", "https://telegra.ph/file/9f8e73d387f25b7f27ce5.jpg")
-    ALIVE_TEXT = getenv("ALIVE_TEXT", "Hey, Saya AyiinUbot Dibuat dengan basis pyrogram versi terbaru")
+    ALIVE_PIC = getenv("ALIVE_PIC", "https://telegra.ph/file/25191c0ea675c9b171882.jpg")
+    ALIVE_TEXT = getenv("ALIVE_TEXT", "Hey, Saya AmwangPyrobot Dibuat dengan basis pyrogram versi terbaru")
     BOT_TOKEN = getenv("BOT_TOKEN", "")
     BLACKLIST_CHAT = getenv("BLACKLIST_CHAT", None)
     if not BLACKLIST_CHAT:
         BLACKLIST_CHAT = [-1001473548283, -1001675396283]
     LOG_CHAT = int(getenv("LOG_CHAT") or 0)
-    HNDLR = getenv("HNDLR", [".", "!", "*", "^", "-", "?"])
+    HNDLR = getenv("HNDLR", [".", "!", "*", "^", "-", "?", " "])
     DB_URL = getenv("DATABASE_URL", "")
     HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
     HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
